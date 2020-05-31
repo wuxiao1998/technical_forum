84b95cd201d13f2aa9dfc65165d0f371f582285e
csy <992945919@qq.com>
2020-06-01 04:07:03 +0800

    改写用户个人信息页面，功能部分实现

--- technical_forum_frontend/src/components/home/UserInfo.jsx
+++ technical_forum_frontend/src/components/home/UserInfo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {
+  Descriptions, 
+  Badge,
   Form,
   Input,
   Select,
@@ -7,8 +9,10 @@ import {
   Button,
   Radio,
   AutoComplete,
+  Modal
 } from 'antd';
 import Axios from 'axios';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 const { Option } = Select;
 const formItemLayout = {
   labelCol: {
@@ -40,8 +44,11 @@ class UserInfo extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-          user:JSON.parse(sessionStorage.getItem("user"))
+          user:JSON.parse(sessionStorage.getItem("user")),
+          visible:false
         }
+        this.showModa=this.showModa.bind(this);
+        this.hideModal=this.hideModal.bind(this);
     }
 
 
@@ -55,11 +62,12 @@ class UserInfo extends React.Component{
     onFinish = values => {
         console.log('Received values of form: ', values);
         Axios.post('http://localhost:8000/forum/user/UserInfo',{
-            username:values.username,
-            gender:values.gender
+            nickname:values.nickname,
+            gender:values.gender,
+            phone:values.phone,
         }).then(res=>{
-            console.log(res);
-            this.props.history.push('/success');
+            console.log(res,11111111111);
+            this.props.history.push('/home/main');
         }).catch(response=>{
             console.log(response);
         })
@@ -74,11 +82,62 @@ class UserInfo extends React.Component{
           value: e.target.value,
         });
       };
+
+      showModa(){
+        this.setState({
+          visible: true,
+        });
+      };
+    
+      hideModal(){
+        this.setState({
+          visible: false,
+        });
+      };
+    
     render(){
       console.log(this.state.user)
-        return <div >
+        return <div style={{marginTop:"5%"}}>
+          <Modal
+          title="提醒"
+          visible={this.state.visible}
+          onOk={this.hideModal}//这里之后要传值
+          onCancel={this.hideModal}
+          okText="确认"
+          cancelText="取消"
+        >
+          <p>是否确认修改</p>
+        </Modal>
         <h1 style={{textAlign:"center"}}>个人信息</h1>
-        <Form
+        <Descriptions   bordered style={{backgroundColor:"white"}}>
+    <Descriptions.Item label="用户名"  span={3}>{this.state.user.username}</Descriptions.Item >
+    <Descriptions.Item label="邮箱" span={3}>{this.state.user.email}</Descriptions.Item>
+    <Descriptions.Item label="昵称"  span={3}><Input id="nickname" defaultValue={this.state.user.nickname} style={{width:"300px"}}></Input></Descriptions.Item>
+    <Descriptions.Item label="经验">{this.state.user.experience}</Descriptions.Item>
+    <Descriptions.Item label="等级">{this.state.user.level}</Descriptions.Item>
+    <Descriptions.Item label="称号">{this.state.user.designation}</Descriptions.Item>
+    <Descriptions.Item label="性别" span={3}>
+        <Radio.Group id="gender" defaultValue={this.state.user.gender}> 
+          <Radio value={1}>男</Radio>
+          <Radio value={2}>女</Radio>
+        </Radio.Group>
+    </Descriptions.Item>
+    <Descriptions.Item label="手机号" ><Input  id="phone" defaultValue={this.state.user.phone} style={{width:"300px"}}></Input></Descriptions.Item>
+  </Descriptions>
+  <div style={{textAlign:"center"}}>
+  <Button type="primary" onClick={this.showModa.bind(this)}>
+            修改
+          </Button>
+          <Button style={{marginLeft:"20px"}} onClick={this.goBack}>
+            返回
+          </Button>
+          </div>
+
+
+
+
+
+        {/* <Form
         {...formItemLayout}
         name="UserInfo"
         style={{marginRight:"300px",marginTop:"50px"}}
@@ -123,7 +182,7 @@ class UserInfo extends React.Component{
             返回
           </Button>
         </Form.Item>
-      </Form>
+      </Form> */}
       </div> 
     }
 
