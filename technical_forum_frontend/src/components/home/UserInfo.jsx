a32c742d4610c8f6bc768aeaf73d7ea95b04d57a
csy <992945919@qq.com>
2020-06-02 04:38:44 +0800

    个人信息页面修改功能完善

--- technical_forum_frontend/src/components/home/UserInfo.jsx
+++ technical_forum_frontend/src/components/home/UserInfo.jsx
@@ -45,11 +45,16 @@ class UserInfo extends React.Component{
         super(props)
         this.state = {
           user:JSON.parse(sessionStorage.getItem("user")),
-          visible:false
+          visible:false,
+          nickname:JSON.parse(sessionStorage.getItem("user"))!==null?JSON.parse(sessionStorage.getItem("user")).nickname:"",
+          phone:JSON.parse(sessionStorage.getItem("user"))!==null?JSON.parse(sessionStorage.getItem("user")).phone:"",
+          gender:JSON.parse(sessionStorage.getItem("user"))!==null?JSON.parse(sessionStorage.getItem("user")).gender:"",
         }
         this.showModa=this.showModa.bind(this);
         this.hideModal=this.hideModal.bind(this);
+        console.log(this.state)
     }
+
 
 
     componentWillMount(){
@@ -60,26 +65,48 @@ class UserInfo extends React.Component{
   
 
     onFinish = values => {
+      this.setState({
+        visible: false,
+      });
         console.log('Received values of form: ', values);
-        Axios.post('/user/UserInfo',{
-            nickname:values.nickname,
-            gender:values.gender,
-            phone:values.phone,
+        Axios.post('/user/updateUser',{
+            nickname:document.getElementById("nickname").value,
+            gender:this.state.gender,
+            phone:document.getElementById("phone").value,
         }).then(res=>{
             console.log(res,11111111111);
-            this.props.history.push('/home/homepage');
+            
+            //this.props.history.push('/home/homepage');
         }).catch(response=>{
             console.log(response);
         })
+        sessionStorage.removeItem("user");
+        this.setState({
+            login:null
+        })
+        console.log(this)
+        this.props.history.push('/login');
+        // Axios.post("/user/login",{                        信息更新后页面显示信息不更新，无法解决，让用户更新完强制重新登陆
+        //   username:this.state.user.username,
+        //   password:this.state.user.password
+        // }).then((response)=>{
+        //   this.setState({
+        //     nickname: response.data.nickname,
+        //     gender:response.data.gender,
+        //     phone:response.data.phone,
+        //   })
+        //   console.log(response,"12345")
+        //   console.log(this.state,'更新后的值')
+        // })
       };
 
     goBack = () =>{
        this.props.history.goBack();
     }
-    onChange = e => {
+    getValue = e => {
         console.log('radio checked', e.target.value);
         this.setState({
-          value: e.target.value,
+          gender: e.target.value,
         });
       };
 
@@ -97,33 +124,34 @@ class UserInfo extends React.Component{
     
     render(){
       console.log(this.state.user)
+      console.log(this.state.gender)
         return <div style={{marginTop:"5%",minHeight: '80vh'}}>
           {this.state.user && <div>
           <Modal
           title="提醒"
           visible={this.state.visible}
-          onOk={this.hideModal}//这里之后要传值
+          onOk={this.onFinish}//这里之后要传值
           onCancel={this.hideModal}
           okText="确认"
           cancelText="取消"
         >
-          <p>是否确认修改</p>
+          <p>修改后需要重新登录，是否确认修改</p>
         </Modal>
         <h1 style={{textAlign:"center"}}>个人信息</h1>
         <Descriptions   bordered style={{backgroundColor:"white"}}>
     <Descriptions.Item label="用户名"  span={3}>{this.state.user.username}</Descriptions.Item >
     <Descriptions.Item label="邮箱" span={3}>{this.state.user.email}</Descriptions.Item>
-    <Descriptions.Item label="昵称"  span={3}><Input id="nickname" defaultValue={this.state.user.nickname} style={{width:"300px"}}></Input></Descriptions.Item>
+    <Descriptions.Item label="昵称"  span={3}><Input id="nickname" defaultValue={this.state.nickname} style={{width:"300px"}}></Input></Descriptions.Item>
     <Descriptions.Item label="经验">{this.state.user.experience}</Descriptions.Item>
     <Descriptions.Item label="等级">{this.state.user.level}</Descriptions.Item>
     <Descriptions.Item label="称号">{this.state.user.designation}</Descriptions.Item>
     <Descriptions.Item label="性别" span={3}>
-        <Radio.Group id="gender" defaultValue={this.state.user.gender}> 
-          <Radio value={1}>男</Radio>
-          <Radio value={2}>女</Radio>
+        <Radio.Group name="gender" defaultValue={this.state.gender}> 
+          <Radio value={1}  onChange={(e)=>this.getValue(e)} >男</Radio>
+          <Radio value={2}  onChange={(e)=>this.getValue(e)} >女</Radio>
         </Radio.Group>
     </Descriptions.Item>
-    <Descriptions.Item label="手机号" ><Input  id="phone" defaultValue={this.state.user.phone} style={{width:"300px"}}></Input></Descriptions.Item>
+    <Descriptions.Item label="手机号" ><Input  id="phone" defaultValue={this.state.phone} style={{width:"300px"}}></Input></Descriptions.Item>
   </Descriptions>
   <div style={{textAlign:"center"}}>
   <Button type="primary" onClick={this.showModa.bind(this)}>
