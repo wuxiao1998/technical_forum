b1d902447a0bd87df770cfa05f279ea299bceffe
cai.shengye <992945919@qq.com>
2020-06-16 03:53:25 +0800

    将菜单栏“我的帖子”改为“我的论坛”，我的论坛页面内实现展示与用户相关帖子，修改用户信息页面背景样式

--- technical_forum_frontend/src/components/Layout/MyHeader.jsx
+++ technical_forum_frontend/src/components/Layout/MyHeader.jsx
@@ -37,7 +37,7 @@ class MyHeader extends React.Component {
                 </div>
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[window.location.hash.split('/')[2]]} >
                 <Menu.Item key="homepage"><Link to={'/home/homepage/' + sessionStorage.getItem("plateKey")}>首页</Link></Menu.Item>
-                <Menu.Item key="mypost"><Link to='/home/mypost'>我的贴子</Link></Menu.Item>
+                <Menu.Item key="mypost"><Link to='/home/mypost'>我的论坛</Link></Menu.Item>
                 <Menu.Item key="userinfo"><Link to='/home/userinfo' >用户信息</Link></Menu.Item>
                 <Menu.Item key="4" style={{ float: "right" }} onClick={this.distoryUser}>{this.state.login ? '注销' : ''}</Menu.Item>
                 {!this.state.login &&<Menu.Item key="5" style={{ float: "right" }}><Link to='/register'>注册</Link></Menu.Item>}
--- technical_forum_frontend/src/components/home/MyPost.jsx
+++ technical_forum_frontend/src/components/home/MyPost.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Result, Button } from 'antd';
-import NoLogin from '../authentication/NoLogin'
+import { Tabs } from 'antd';
+import NoLogin from '../authentication/NoLogin';
+import MyPostList from'./forumpost/MyPostList';
+const { TabPane } = Tabs;
 //我的帖子组件
 class MyPost extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            user: JSON.parse(sessionStorage.getItem("user")),
             loginin: true
         }
     }
 
+
     componentWillMount() {
+        document.body.style.backgroundColor = '#F0F2F5';//将整个页面背景色改为灰色，解决有背景没被渲染出现白色；（副作用容易造成重绘影响性能，暂未发现影响）
         if (!sessionStorage.getItem("user")) {
 
             this.setState({
@@ -26,12 +32,35 @@ class MyPost extends React.Component {
 
     render() {
         let element;
-        if (this.state.loginin) {
-            element = <div>我的帖子</div>
+        console.log(this.state.user,'987')
+        if (this.state.loginin) {            
+            element = <div>
+                <Tabs defaultActiveKey="1" style={{backgroundColor:"#F0F2F5"}} type="line" size={"large"}>
+    <TabPane
+      tab={
+        <span>
+          &nbsp;&nbsp;我的帖子
+        </span>
+      }
+      key="1"
+    >
+      <MyPostList ></MyPostList>
+    </TabPane>
+    <TabPane
+      tab={
+        <span>
+          我的消息
+        </span>
+      }
+      key="2"
+    >我的消息
+    </TabPane>
+  </Tabs>
+                </div >
         } else {
             element = <NoLogin history={this.props.history}></NoLogin>
         }
-        return <div style={{ minHeight: '80vh', marginTop: "5%" }}>
+        return <div style={{ minHeight: '80vh', marginTop: "3%", marginLeft: "3%" , marginRight: "3%", marginBottom: "3%",}}>
             {element}
         </div>
     }
