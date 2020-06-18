a3554a5d070d1419f107777bd69918b18812097d
wu.xiao <825612027@qq.com>
2020-06-18 16:34:42 +0800

    前端项目启动时,增加默认重定向主页配置

--- technical_forum_frontend/src/components/home/HomeRouter.jsx
+++ technical_forum_frontend/src/components/home/HomeRouter.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import HomePage from './HomePage'
 import MyPost from './MyPost'
 import UserInfo from './UserInfo'
@@ -31,12 +31,13 @@ class HomeRouter extends React.Component {
         <div className="site-layout-background" style={{ minHeight: 380, height: '620px' }}>
           <Switch>
             {/*这里配置主页的子路由信息,组件会被渲染中主页的中间部分,保留头部和底部*/}
-            <Route exact path='/' component={HomePage}></Route>
             <Route exact path='/home/homepage/:id' component={HomePage}></Route>
             <Route exact path='/home/mypost' component={MyPost}></Route>
             <Route exact path='/home/userinfo' component={UserInfo}></Route>
             <Route exact path='/home/noticemanagement' component={NoticeManagement}></Route>
             <Route exact path='/home/wholenotice' component={WholeNotice}></Route>
+            {/* 重定向至主页,此匹配项最好放最后*/ }
+            <Redirect from='/' to='/home/homepage/1'></Redirect>
           </Switch>
             {/** 导入页面底部*/}
           <MyFooter></MyFooter>
